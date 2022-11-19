a7c950a67a2a891ec6c6c75ceb126a64d7b24a54
Wiphawanyaun <gitwiphawan@gmail.com>
2022-11-19 12:02:42 +0700

    workshopB

---
+++ workshopB.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    name: "Wiphawan",
+    nickname: "Yaun",
+    hobby: "Playing Game",
+    address: "22/13 ",
+    province: { name: "Bangkok", postcode: 10220 },
+  },
+  {
+    name: "Wiphawan",
+    nickname: "Yuan",
+    hobby: "Eating",
+    address: "22/13 ",
+    province: { name: "Bangkok", postcode: 10221 },
+  },
+  {
+    name: "Wiphawan",
+    nickname: "Ynua",
+    hobby: "Sleep",
+    address: "22/13 ",
+    province: { name: "Bangkok", postcode: 10222 },
+  },
+];
+
+for (const u of users) {
+  console.log(`"My nickname is ${u.nickname} ,My hobby is ${u.hobby}
+  and my postcode is ${u.province.postcode}"`);
+}
